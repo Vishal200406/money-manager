64ad3983312d09535d93f605d5a96b4dce641897
Vishal200406 <malhotravishal101@gmail.com>
2026-08-20 13:52:12 +0300

    add colors to expense pie chart

--- client/src/components/ExpensePieChart.tsx
+++ client/src/components/ExpensePieChart.tsx
@@ -3,17 +3,17 @@
 
 import {
 
-PieChart,
+  PieChart,
 
-Pie,
+  Pie,
 
-Cell,
+  Cell,
 
-Tooltip,
+  Tooltip,
 
-Legend,
+  Legend,
 
-ResponsiveContainer
+  ResponsiveContainer
 
 } from "recharts";
 
@@ -21,119 +21,210 @@ ResponsiveContainer
 
 interface Props {
 
-data:any[];
+  data:any[];
 
 }
+
+
+
+
+
+const COLORS = [
+
+  "#2563eb",
+
+  "#16a34a",
+
+  "#dc2626",
+
+  "#f59e0b",
+
+  "#9333ea",
+
+  "#0891b2",
+
+];
+
+
+
+
 
 
 
 export default function ExpensePieChart({
 
-data
+  data
 
 }:Props){
 
 
 
-return (
-
-<div
-
-className="
-bg-white
-border
-rounded-xl
-p-6
-h-96
-"
-
->
 
 
-<h2
-
-className="
-text-xl
-font-semibold
-mb-4
-"
-
->
-
-Expenses By Category
-
-</h2>
+  return (
 
 
 
-<ResponsiveContainer
+    <div
 
-width="100%"
+      className="
+      bg-white
+      border
+      rounded-xl
+      p-6
+      h-96
+      "
 
-height="85%"
-
->
-
-
-<PieChart>
-
-
-<Pie
-
-data={data}
-
-dataKey="amount"
-
-nameKey="name"
-
-cx="50%"
-
-cy="50%"
-
-outerRadius={100}
-
-label
-
->
-
-
-{
-
-data.map(
-
-(entry,index)=>(
-
-<Cell
-
-key={`cell-${index}`}
-
-/>
-
-)
-
-)
-
-}
-
-
-</Pie>
+    >
 
 
 
-<Tooltip />
 
 
-<Legend />
+      <h2
+
+        className="
+        text-xl
+        font-semibold
+        mb-4
+        "
+
+      >
+
+        Expenses By Category
 
 
-</PieChart>
+      </h2>
 
 
-</ResponsiveContainer>
 
 
-</div>
 
-);
+
+
+
+
+      <ResponsiveContainer
+
+        width="100%"
+
+        height="85%"
+
+      >
+
+
+
+
+        <PieChart>
+
+
+
+
+
+          <Pie
+
+            data={data}
+
+            dataKey="amount"
+
+            nameKey="name"
+
+            cx="50%"
+
+            cy="50%"
+
+            outerRadius={100}
+
+            label
+
+
+          >
+
+
+
+
+
+            {
+
+
+              data.map(
+
+
+                (entry,index)=>(
+
+
+
+                  <Cell
+
+
+                    key={`cell-${index}`}
+
+
+                    fill={
+
+                      COLORS[
+
+                        index %
+
+                        COLORS.length
+
+                      ]
+
+                    }
+
+
+                  />
+
+
+
+                )
+
+
+              )
+
+
+            }
+
+
+
+
+
+
+          </Pie>
+
+
+
+
+
+
+
+          <Tooltip />
+
+
+
+          <Legend />
+
+
+
+
+
+        </PieChart>
+
+
+
+
+
+      </ResponsiveContainer>
+
+
+
+
+
+    </div>
+
+
+
+  );
+
 
 }
