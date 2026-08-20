d1dab00996e077fd5379353776d73fa396e152ee
Vishal200406 <malhotravishal101@gmail.com>
2026-08-20 14:01:30 +0300

    fix report category names using transaction category data

--- server/src/controllers/reportController.ts
+++ server/src/controllers/reportController.ts
@@ -1,174 +1,156 @@
 import { Request, Response } from "express";
+
 import Transaction from "../models/Transaction";
+
 
 
 export const getReports = async (
 
-req:Request,
+  req: Request,
 
-res:Response
+  res: Response
 
-)=>{
+) => {
 
 
-try{
+  try {
 
 
-const userId = (req as any).user.id;
+    const userId = (req as any).user.id;
 
 
-const period =
-req.query.period || "month";
+    const period =
 
+      req.query.period || "month";
 
 
-let startDate = new Date();
 
 
 
-if(period==="year"){
+    let startDate = new Date();
 
-startDate.setFullYear(
 
-startDate.getFullYear()-1
 
-);
 
-}
 
-else if(period==="month"){
+    if(period === "year"){
 
-startDate.setMonth(
 
-startDate.getMonth()-1
+      startDate.setFullYear(
 
-);
+        startDate.getFullYear() - 1
 
-}
+      );
 
-else{
 
-startDate = new Date(0);
+    }
 
-}
+    else if(period === "month"){
 
 
+      startDate.setMonth(
 
+        startDate.getMonth() - 1
 
+      );
 
-const transactions = await Transaction.find({
 
-userId,
+    }
 
-date:{
+    else{
 
-$gte:startDate
 
-}
+      startDate = new Date(0);
 
-});
 
+    }
 
 
 
 
-let income = 0;
 
-let expenses = 0;
 
 
-const categories:any = {};
 
 
+    const transactions = await Transaction.find({
 
+      userId,
 
+      date:{
 
-transactions.forEach((transaction:any)=>{
+        $gte:startDate
 
+      }
 
-if(transaction.type==="income"){
 
+    })
 
-income += transaction.amount;
+    .populate(
 
+      "categoryId",
 
-}
+      "name icon"
 
-else{
+    );
 
 
-expenses += transaction.amount;
 
 
 
-const category =
 
-transaction.category?.name ||
 
-"Other";
 
 
+    let income = 0;
 
-if(!categories[category]){
 
+    let expenses = 0;
 
-categories[category]={
 
-name:category,
 
-amount:0
 
-};
+    const categories:any = {};
 
 
-}
 
 
-categories[category].amount +=
 
-transaction.amount;
 
 
-}
 
 
+    transactions.forEach((transaction:any)=>{
 
-});
 
 
 
 
+      if(transaction.type === "income"){
 
-const categoryData =
 
-Object.values(categories).map(
+        income += transaction.amount;
 
-(item:any)=>({
 
+      }
 
-...item,
+      else{
 
 
-percent:
 
-expenses > 0
+        expenses += transaction.amount;
 
-?
 
-Math.round(
 
-(item.amount / expenses)*100
 
-)
 
-:
+        const categoryName =
 
-0
 
+          transaction.categoryId?.name ||
 
-})
 
-);
+          "Other";
 
 
 
@@ -176,40 +158,168 @@ Math.round(
 
 
 
-res.json({
 
-income,
+        if(!categories[categoryName]){
 
-expenses,
 
-savings:
+          categories[categoryName] = {
 
-income-expenses,
 
-categories:categoryData,
+            name:
 
-trend:[]
+              categoryName,
 
-});
 
+            amount:
 
+              0
 
-}
 
-catch(error){
+          };
 
 
-console.error(error);
+        }
 
 
-res.status(500).json({
 
-message:"Failed generating report"
 
-});
 
 
-}
+
+
+        categories[categoryName].amount +=
+
+          transaction.amount;
+
+
+
+      }
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+    const categoryData =
+
+
+      Object.values(categories).map(
+
+        (item:any)=>(
+
+
+          {
+
+
+            ...item,
+
+
+            percent:
+
+              expenses > 0
+
+              ?
+
+              Math.round(
+
+                (
+
+                  item.amount /
+
+                  expenses
+
+                ) * 100
+
+              )
+
+              :
+
+              0
+
+
+          }
+
+
+
+        )
+
+
+      );
+
+
+
+
+
+
+
+
+
+    return res.json({
+
+
+      income,
+
+
+      expenses,
+
+
+      savings:
+
+        income - expenses,
+
+
+      categories:
+
+        categoryData,
+
+
+      trend:[]
+
+
+    });
+
+
+
+
+
+
+  }
+
+  catch(error){
+
+
+
+    console.error(
+
+      "Report error:",
+
+      error
+
+    );
+
+
+
+    return res.status(500).json({
+
+
+      message:
+
+        "Failed generating report"
+
+
+    });
+
+
+
+  }
 
 
 
